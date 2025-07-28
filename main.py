9be69be6c5ae56b966d3483da4adbfad232193c9
Preston Yoshino <preston.yoshino@servco.com>
2025-07-28 08:32:31 -1000

    added glazegraph class working on functions

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-
+# initial scars gc 
 friendGroup = {
-    "Colson": ["Nasir", "Colin"],
-    "Justice": ["Colson", "Preston", "Nasir"],
-    "Carlos": ["Luke", "Colson", "Justice", "Kai", "Brennan"],
-    "Nasir": ["Preston"],
-    "Brennan": ["Colson", "Justice", "Nasir", "Carlos", "Kai"],
-    "Luke": ["Colson", "Kai", "Colin"],
-    "Kai": ["Carlos", "Colson", "Justice", "Luke"],
-    "Colin": ["Colson", "Justice", "Nasir", "Nate"],
-    "Nate": ["Preston", "Colson", "Justice"]
-}
+        "Colson": ["Nasir", "Colin"],
+        "Justice": ["Colson", "Preston", "Nasir"],
+        "Carlos": ["Luke", "Colson", "Justice", "Kai", "Brennan"],
+        "Nasir": ["Preston"],
+        "Brennan": ["Colson", "Justice", "Nasir", "Carlos", "Kai"],
+        "Luke": ["Colson", "Kai", "Colin"],
+        "Kai": ["Carlos", "Colson", "Justice", "Luke"],
+        "Colin": ["Colson", "Justice", "Nasir", "Nate"],
+        "Nate": ["Preston", "Colson", "Justice"],
+    }
+
+class glazeTree:
+    def __init__(self, graph=None):
+        self.graph = graph if graph else {}
+
+    def add(self, name, glazees=[], glazers=[]):
+        self.graph[name] = glazees
+        for glazer in glazers:
+            self.graph[glazer].append(name)
+            print("{glazer} is now a glazer of {name}".format(glazer, name))
+
