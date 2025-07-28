d47bfac2068bc65b440d47422f8c5941b0d03bcf
Preston Yoshino <preston.yoshino@servco.com>
2025-07-28 08:43:33 -1000

    7/28 progress addes str and add methods

--- main.py
+++ main.py
@@ -11,13 +11,31 @@ friendGroup = {
         "Nate": ["Preston", "Colson", "Justice"],
     }
 
-class glazeTree:
+class glazeGraph:
     def __init__(self, graph=None):
         self.graph = graph if graph else {}
 
-    def add(self, name, glazees=[], glazers=[]):
+    def add(self, name, glazees=None, glazers=None):
+        #checking arguments
+        if glazees is None:
+            glazees = []
+        if glazers is None:
+            glazers = []
+        #adding new friend to graph
         self.graph[name] = glazees
-        for glazer in glazers:
-            self.graph[glazer].append(name)
-            print("{glazer} is now a glazer of {name}".format(glazer, name))
+        for glazee in glazees:
+            print(f"{glazee} gets glazed by {name}")
 
+        #adjusting relationships
+        for glazer in glazers:
+            if glazer not in self.graph:
+                self.graph[glazer] = []
+            if name not in self.graph[glazer]:
+                self.graph[glazer].append(name)
+            print(f"{glazer} is now a glazer of {name}")
+
+def main():
+    scars = glazeGraph(friendGroup)
+    scars.add("Kaili", ["Preston", "Nasir", "Colson", "Luke"], ["Nasir", "Brennan", "Kai"])
+
+main()
