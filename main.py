efd1c60494596966c95aa94ed0be5ac51b58d219
Preston Yoshino <preston.yoshino@servco.com>
2025-07-28 08:15:03 -1000

    initialize dictionary

---
+++ main.py
@@ -0,0 +1,12 @@
+
+friendGroup = {
+    "Colson": ["Nasir", "Colin"],
+    "Justice": ["Colson", "Preston", "Nasir"],
+    "Carlos": ["Luke", "Colson", "Justice", "Kai", "Brennan"],
+    "Nasir": ["Preston"],
+    "Brennan": ["Colson", "Justice", "Nasir", "Carlos", "Kai"],
+    "Luke": ["Colson", "Kai", "Colin"],
+    "Kai": ["Carlos", "Colson", "Justice", "Luke"],
+    "Colin": ["Colson", "Justice", "Nasir", "Nate"],
+    "Nate": ["Preston", "Colson", "Justice"]
+}
